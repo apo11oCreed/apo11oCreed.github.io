269357e0a48b09f9121904927c993ef0a5118d96
nelson <nelson@ncdesigns-studio.com>
2016-02-02 23:40:41 -0500

    changed to rouge and configured serviceWorker

--- assets/themes/bootstrap-3/bootstrap/js/serviceWorker.js
+++ assets/themes/bootstrap-3/bootstrap/js/serviceWorker.js
@@ -1,7 +1,16 @@
 self.addEventListener('install', event =>{
-	
+	function onInstall(){
+		return caches.open('static')
+		.then(cache =>
+			cache.addAll([
+				'/'
+				])
+			);
+	}
+		event.waitUntil(onInstall(event));
 });
 
 self.addEventListener('activate', event =>{
+	console.log('installing');
 	
 });
