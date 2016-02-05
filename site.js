4661ffd8f1e03ed57089248fb6398830cfe29a9f
nelson <nelson@ncdesigns-studio.com>
2016-02-04 21:11:51 -0500

    relocated site.js and serviceWorker.js for scope

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
 	navigator.serviceWorker.register('/assets/themes/bootstrap-3/bootstrap/js/serviceWorker.js',{
-		scope: '/../../../../../../'
+		scope: '/'
 	});
 }
