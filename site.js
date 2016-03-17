905396b99fc4aa4f84e6aabcd6dc5a4508e62f9a
nelson <nelson@ncdesigns-studio.com>
2016-03-16 21:37:12 -0400

    changed relative to absolute directory of serviceWorker.js

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-	navigator.serviceWorker.register('serviceWorker.js',{
+	navigator.serviceWorker.register('https://ncdesigns-studio.com/serviceWorker.js',{
 		scope: '/'
 	});
 }
