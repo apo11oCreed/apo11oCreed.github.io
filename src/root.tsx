fc8994cb410346348662f002c311f2673c89f81a
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-09-27 00:25:27 -0400

    config(core): apply changes to global stylesheet and page layout meta

--- src/root.tsx
+++ src/root.tsx
@@ -1,16 +1,21 @@
-import { Counter } from "./components/counter/counter";
-import { Logo } from "./components/logo/logo";
+// import { Counter } from "./components/counter/counter";
+// import { Logo } from "./components/logo/logo";
+import './global.css';
 
 export default () => {
   return (
     <>
       <head>
         <meta charset="utf-8" />
-        <title>Qwik Blank App</title>
+        <title>Nelson Correia</title>
       </head>
       <body>
-        <Logo />
-        <Counter />
+        {/* <Logo />
+        <Counter /> */}
+        <main>
+          <h1 class="text-4xl font-bold underline">Hello world! This is Nelson Correia</h1>
+          <p>My new site is currently under construction. Stay tuned for updates!</p>
+        </main>
       </body>
     </>
   );
