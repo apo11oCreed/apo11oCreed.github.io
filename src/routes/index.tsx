9e063c82c3b2e9c1b9caf0c9295e597f1140fa09
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-10-03 01:08:24 -0400

    styles(fonts): add brand font - ubuntu - and starter content

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,22 +4,29 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+    <main>
+        <section>
+          <h1>Hi, I'm Nelson Correia</h1>
+          <p>
+            Front-end developer crafting modern, performant web experiences with 
+            Svelte, TypeScript, and cutting-edge frameworks. I transform ideas into 
+            pixel-perfect, user-focused applications.
+          </p>
+          <p>
+            Let's build something exceptional together.
+          </p>
+        </section>
+      </main>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Nelson correia - Front-end Developer",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Portfolio website of Nelson Correia",
     },
   ],
 };
