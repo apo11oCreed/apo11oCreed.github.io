6ed704322e03709549ffde189de8559bfa02e314
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-10-12 23:26:05 -0400

    fix(ts): set qwik script as string instead of object

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -46,13 +46,11 @@ export const RouterHead = component$(() => {
       <meta property="og:url" content={loc.url.href} />
 
       {/* Google Tag Manager */}
-      <script dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-W79S8P');`
-      }}/>
+      <script dangerouslySetInnerHTML={`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-W79S8P');`}/>
       {/* End Google Tag Manager */}
     </>
   );
