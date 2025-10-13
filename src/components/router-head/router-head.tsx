0ea91c1bd84b0e4eed45a766ca670bc8bcb5b035
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-10-12 23:19:18 -0400

    configs(gtm): gtm script needs to be wrapped in qwik-specific script tag

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -45,10 +45,15 @@ export const RouterHead = component$(() => {
       <meta property="og:title" content={title} />
       <meta property="og:url" content={loc.url.href} />
 
-      {/* Google Tag Manager (noscript) */}
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W79S8P"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-      {/* End Google Tag Manager (noscript) */}
+      {/* Google Tag Manager */}
+      <script dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W79S8P');`
+      }}/>
+      {/* End Google Tag Manager */}
     </>
   );
 });
