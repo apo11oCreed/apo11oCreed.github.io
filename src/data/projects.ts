7b3d6565cc9d7198154189045184fca4ef4a3787
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-12-01 11:25:05 -0500

    fix(content): adjust project title

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@
 
 export const projects = [
   {
-    name: "The Future of Artificial Intelligence Policy Is the Future of Competing Demands",
+    name: "Scrollytelling Interactive Visualization",
     description: "Developed a scrollytelling experience with animated, data-driven visualizations and seamlessly integrated a third-party application into the SPA. Ensured consistent performance and compatibility across major browsers and both desktop and mobile devices.",
     url: "https://www.rand.org/pubs/visualizations/DVA3888-1.html",
     stack: ["Vite","Svelte", "TypeScript", "CSS"],
