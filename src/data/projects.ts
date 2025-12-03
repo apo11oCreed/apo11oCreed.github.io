bfb2763980cc1d4c4423a934169013afb9c26ee1
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-12-03 11:17:31 -0500

    feat(content): add new project data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,9 +4,26 @@ export const projects = [
   {
     name: "Scrollytelling Interactive Visualization",
     description: "Developed a scrollytelling experience with animated, data-driven visualizations and seamlessly integrated a third-party application into the SPA. Ensured consistent performance and compatibility across major browsers and both desktop and mobile devices.",
+    credit: "Project completed while serving as Web Applications Developer at RAND Corporation.",
     url: "https://www.rand.org/pubs/visualizations/DVA3888-1.html",
-    stack: ["Vite","Svelte", "TypeScript", "CSS"],
+    stack: ["Yarn","Vite","Svelte", "TypeScript", "SCSS", "D3.js", "Scrollama"],
     img: "interactive-app-visualization-2.png"
+  },
+  {
+    name: "Data-Driven Interactive Tool",
+    description: "Extended an existing data visualization application by integrating new datasets and enhancing user interactivity. Tooltips and more granular views were added to promote further data exploration.",
+    credit: "Project completed while serving as Web Applications Developer at RAND Corporation.",
+    url: "https://www.rand.org/research/gun-policy/law-navigator.html",
+    stack: ["NPM", "Webpack", "SCSS", "D3.js"],
+    img: "interactive-app-tool-3.png"
+  },
+  {
+    name: "Data-Driven Interactive Visualization",
+    description: " Extended an existing data visualization application by integrating new datasets and enhancing user interactivity. Tooltips and more granular views were added to promote further data exploration.",
+    credit: "Project completed while serving as Web Applications Developer at RAND Corporation.",
+    url: "https://www.rand.org/pubs/visualizations/DVA1544-2/logic-model-for-intermediate-force-capabilities.html",
+    stack: ["Yarn", "Vite", "Tailwind CSS"],
+    img: "interactive-app-visualization-1.png"
   }
 ] as const;
 
