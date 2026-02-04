9b05092dadb7d4c18643560ec52a978fda197499
Nelson Correia <nelson.ncdesigns@gmail.com>
2026-02-04 16:30:40 -0500

    styles(headings): move css from global to component module

--- src/components/heading/heading.tsx
+++ src/components/heading/heading.tsx
@@ -1,10 +1,11 @@
 import { component$, Slot } from '@builder.io/qwik';
+import styles from './styles.module.css';
 
-interface HeadingProps {
+export interface HeadingProps {
   level?: 1 | 2 | 3 | 4 | 5 | 6;
 }
 
 export const Heading = component$<HeadingProps>(({ level = 1 }) => {
   const Tag = `h${level}` as 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-  return <Tag><Slot /></Tag>;
+  return <Tag class={styles.heading}><Slot /></Tag>;
 });            
