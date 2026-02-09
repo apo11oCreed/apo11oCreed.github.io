49cee42c9b59f1325fd3e260c66d0008383ab595
Nelson Correia <nelson.ncdesigns@gmail.com>
2026-02-09 17:01:44 -0500

    feat(heading): refactor meta for common props and refine UI labels

--- src/components/heading/heading.stories.tsx
+++ src/components/heading/heading.stories.tsx
@@ -3,18 +3,17 @@ import { Heading, type HeadingProps } from "./heading";
 
 const meta: Meta<HeadingProps> = {
   component: Heading,
-  title: "Content/Heading",
-};
-
-type Story = StoryObj<HeadingProps>;
-
-export default meta;
-
-export const Headings: Story = {
+  title: "Content/Headings",
+  parameters: {
+    docs: {
+      description: {
+        component: '[View Figma Design](https://www.figma.com/design/8YzC8yJDF7sHWM9ou4DdNO/Website?node-id=1-4&m=dev&t=DXfABXPVy9ImwzC0-1)',
+      },
+    },
+  },
   args: {
     level: 1,
     text: 'Some heading',
-    mode: 'light',
   },
   argTypes: {
     level: {
@@ -30,4 +29,20 @@ export const Headings: Story = {
     }
   },
   render: (props: HeadingProps) => <Heading {...props}>{props.text}</Heading>,
+};
+
+type Story = StoryObj<HeadingProps>;
+
+export default meta;
+
+export const LightMode: Story = {
+  args: {
+    mode: 'light',
+  },
+};
+
+export const DarkMode: Story = {
+  args: {
+    mode: 'dark',
+  },
 };
